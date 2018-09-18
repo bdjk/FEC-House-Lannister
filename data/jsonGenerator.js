36d04e2bb01e6db3d546f64bde99222cbbc4b06f
Alexander Lee <alixlii@Alexanders-MacBook-Air.local>
2018-09-18 15:28:39 -0700

    New data and added json generator template for reference

---
+++ data/jsonGenerator.js
@@ -0,0 +1,17 @@
+[
+  '{{repeat(100)}}',
+  {
+    name: '{{firstName()}}',
+    picture: 'https://sephora.i.lithium.com/t5/image/serverpage/avatar-name/default-avatar/avatar-theme/sephora/avatar-collection/sephora/avatar-display-size/profile/version/2?xdesc=1.0',
+    eyeColor: '{{random("blue", "brown", "green", "gray", "hazel")}}',
+    hairColor: '{{random("blonde", "brunette", "auburn", "black", "red", "gray")}}',
+    skinTone: '{{random("porcelain", "fair", "light", "medium", "tan", "olive", "deep", "dark", "ebony")}}',
+    skinType: '{{random("normal", "combination", "dry", "oily")}}',
+    rating: '{{integer(1, 5)}}',
+    message: 'Best product ever! Like OMG!! Fabulous!!!',
+    rank1: "https://community.sephora.com/html/rank_icons/birole_rouge.svg",
+    rank2: "https://community.sephora.com/html/rank_icons/rank_rookie-01.svg",
+    helpful: 0,
+    recommends: false
+  }
+]
